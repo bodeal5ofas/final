c3f87a5cf47403771115c4d31e274884f5b69c23
bodeal5ofas <bolar8596@gmail.com>
2023-10-16 17:47:12 +0300

    create

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:final_project/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +11,10 @@ class FinalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
+      routes: {HomePage.id: (context) => HomePage()},
+      initialRoute: HomePage.id,
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+  static String id = 'HomePage';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes'), actions: [Icon(Icons.search)]),
+    );
   }
 }
