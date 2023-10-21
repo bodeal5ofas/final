1ca4dec6e7333b215f831cfbe0de137ec00e9e31
bodeal5ofas <bolar8596@gmail.com>
2023-10-21 12:38:30 +0300

    create TybeAdabter

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,9 +1,15 @@
-import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
 
-class NoteModel {
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final int color;
+  @HiveField(3)
   final String data;
 
   NoteModel({
